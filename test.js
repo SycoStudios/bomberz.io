6d2904f114f0f23a9bc74bfc4ed74301a018542c
E <84803278+Codezilluh@users.noreply.github.com>
2022-08-11 15:25:14 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 import { exec } from "child_process";
 
-exec("npm start");
-exec("node website");
-exec("npx parcel src/index.html --no-cache");
+exec("npm start", (err) => {
+	if (err) {
+		console.log("\n✖ error in game server");
+	}
+});
+exec("node website", (err) => {
+	if (err) {
+		console.log("\n✖ error in database server");
+	}
+});
+exec("npx parcel src/index.html --no-cache", (err) => {
+	if (err) {
+		console.log("\n✖ error in dev live reload");
+	}
+});
 
 console.log("✓ opened game server");
 console.log("✓ hosting database server");
