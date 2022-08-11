a6137bbf544b333fbd4304ca7efaf94c8305068f
E <84803278+Codezilluh@users.noreply.github.com>
2022-08-11 15:21:09 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ exec("npm start");
 exec("node website");
 exec("npx parcel src/index.html --no-cache");
 
-console.log("opened game server");
-console.log("hosting database server");
-console.log("hosting dev live reload");
+console.log("✓ opened game server");
+console.log("✓ hosting database server");
+console.log("✓ hosting dev live reload");
